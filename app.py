130d335fb48f667f86f1f93307b83747cf7ef3ec
Huglean_Link <6pp4shpkjj@privaterelay.appleid.com>
2025-11-21 08:17:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,25 @@ import streamlit as st
 from utils.loader import list_airports, list_years, load_metar
 from plot.temp import plot_temp
 
-st.title("METAR 温度热图")
-
+st.set_page_config(page_title="气象数据可视化",layout="wide")
+st.title("气象数据可视化系统")
+choice = st.selectbox("选择数据类型",["气温热图","露点热图","风向热图","风速热图","风玫瑰"])
 airport = st.selectbox("选择机场", list_airports())
 year = st.selectbox("选择年份", list_years(airport))
-df = load_metar(airport, year)
-fig = plot_temp(df, airport, year)
-st.pyplot(fig)
+if st.button("查询图像"):
+    df = load_metar(airport, year)
+    if choice == "气温热图":
+        fig = plot_temp(df, airport, year)
+    elif choice == "露点热图":
+        st.warning("露点模块尚未完成")
+        st.stop()
+    elif choice == "风向热图":
+        st.warning("风向模块尚未完成")
+        st.stop()
+    elif choice == "风速热图":
+        st.warning("风速模块尚未完成")
+        st.stop()
+    elif choice == "风玫瑰":
+        st.warning("风玫瑰模块尚未完成")
+        st.stop()
+    st.pyplot(fig)
