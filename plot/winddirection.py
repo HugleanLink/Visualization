54649b85d8b680ca7362cbb2f1fa268491feb94e
Huglean_Link <6pp4shpkjj@privaterelay.appleid.com>
2025-11-21 21:38:01 +0800

    Update winddirection.py

--- plot/winddirection.py
+++ plot/winddirection.py
@@ -6,6 +6,7 @@ def plot_winddir(df1,airport,year):
     pivot = df1.groupby(["month", "hour"])["winddir"].mean().unstack()
     fig = plt.figure(figsize=(15, 6))
     sns.heatmap(pivot,cmap="Set3",linewidths=0.3,vmin=0,vmax=360,cbar_kws={"label": "Wind Direction(°)"})
+    cbar = ax.collections[0].colorbar
     cbar.set_yticks([0,30, 60, 90, 120, 150,180,210,240,270,300,330,360])
     cbar.set_yticklabels(["0°", "30°", "60°", "90°", "120°", "150°","180°","210°","240°","270°","300°","330°","360°"])
     plt.title(f"{year} WindDirection HeatMap")
@@ -14,9 +15,3 @@ def plot_winddir(df1,airport,year):
     plt.xticks(range(24), [f"{h:02d}:00" for h in range(24)], rotation=45)
     plt.yticks(rotation=0)
     return fig
-
-
-
-
-
-
