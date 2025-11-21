0e184341312ab3b9c72a037209ac5d7baddc2318
Huglean_Link <6pp4shpkjj@privaterelay.appleid.com>
2025-11-21 15:38:40 +0800

    Add files via upload

---
+++ plot/winddirection.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_winddir(df1,airport,year):
+    pivot = df1.groupby(["month", "hour"])["winddir"].mean().unstack()
+    fig = plt.figure(figsize=(15, 6))
+    sns.heatmap(pivot,cmap="hsv",linewidths=0.3,cbar_kws={"label": "Wind Direction(°)"})
+    plt.title(f"{year} WindDirection HeatMap")
+    plt.xlabel("Time")
+    plt.ylabel("Month")
+    plt.xticks(range(24), [f"{h:02d}:00" for h in range(24)], rotation=45)
+    plt.yticks(rotation=0)
+    return fig
---
+++ plot/windspeed.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_windspeed(df1,airport,year):
+    pivot = df1.groupby(["month", "hour"])["windspeed"].mean().unstack()
+    fig = plt.figure(figsize=(15, 6))
+    sns.heatmap(pivot,cmap="Blues",linewidths=0.3,cbar_kws={"label": "Wind Speed(°)"})
+    plt.title(f"{year} WindDirection HeatMap")
+    plt.xlabel("Time")
+    plt.ylabel("Month")
+    plt.xticks(range(24), [f"{h:02d}:00" for h in range(24)], rotation=45)
+    plt.yticks(rotation=0)
+    return fig
