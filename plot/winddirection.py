3442fb73916489a0b00dc1853a3123c34f65243e
Huglean_Link <6pp4shpkjj@privaterelay.appleid.com>
2025-11-21 16:56:33 +0800

    Update winddirection.py

--- plot/winddirection.py
+++ plot/winddirection.py
@@ -5,13 +5,14 @@ import seaborn as sns
 def plot_winddir(df1,airport,year):
     pivot = df1.groupby(["month", "hour"])["winddir"].mean().unstack()
     fig = plt.figure(figsize=(15, 6))
-    sns.heatmap(pivot,cmap="YlGnBu",linewidths=0.3,vmin=0,vmax=360,cbar_kws={"label": "Wind Direction(°)"})
+    sns.heatmap(pivot,cmap="set3",linewidths=0.3,vmin=0,vmax=360,cbar_kws={"label": "Wind Direction(°)"})
     plt.title(f"{year} WindDirection HeatMap")
     plt.xlabel("Time")
     plt.ylabel("Month")
     plt.xticks(range(24), [f"{h:02d}:00" for h in range(24)], rotation=45)
     plt.yticks(rotation=0)
     return fig
+
 
 
 
