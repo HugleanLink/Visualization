07e66e23743d87e7e6fc0adb3850bfb20f2e3f7e
Huglean_Link <6pp4shpkjj@privaterelay.appleid.com>
2025-11-21 15:38:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import io
-from utils.loader import list_airports, list_years, load_metar,load_dew
+from utils.loader import list_airports, list_years, load_metar,load_dew,load_wind
 from plot.temp import plot_temp
 from plot.dewpoint import plot_dew
+from plot.winddirection import plot_winddir
+from plot.windspeed import plot_windspeed
 from plot.Windrose import plot_wind
 
 
@@ -13,21 +15,21 @@ airport = st.selectbox("选择机场", list_airports())
 year = st.selectbox("选择年份", list_years(airport))
 if st.button("查询图像"):
     df = load_metar(airport, year)
+    df1 = load_wind(airport, year)
     df2 = load_dew(airport, year)
     if choice == "气温热图":
         fig = plot_temp(df, airport, year)
     elif choice == "露点热图":
         fig = plot_dew(df2, airport, year)
     elif choice == "风向热图":
-        st.warning("风向模块尚未完成")
-        st.stop()
+        fig = plot_winddir(df1,airport,year)
     elif choice == "风速热图":
-        st.warning("风速模块尚未完成")
-        st.stop()
+        fig = plot_windspeed(df1,airport,year)
     elif choice == "风玫瑰":
-        fig = plot_wind(df, airport, year)
-        # st.warning("风玫瑰模块尚未完成")
-        # st.stop()
+        st.warning("风玫瑰模块尚未完成")
+        st.stop()
+
+
     st.pyplot(fig)
     buf = io.BytesIO()
     fig.savefig(buf, format="png", dpi=1280)
@@ -38,4 +40,3 @@ if st.button("查询图像"):
         file_name=f"{airport}_{year}_{choice}.png",
         mime="image/png"
     )
-
