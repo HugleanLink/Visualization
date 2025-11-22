0e52222abaad2316ca38e1db596f78e8e4f2cd30
Huglean_Link <6pp4shpkjj@privaterelay.appleid.com>
2025-11-22 19:37:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from plot.Windrose import plot_wind
 
 
 st.set_page_config(page_title="气象数据可视化平台", layout="wide")
-st.title("气象数据可视化系统")
+st.title("气象数据可视化")
 choice = st.selectbox("选择数据类型", ["气温热图", "露点热图", "风向热图", "风速热图", "风玫瑰"])
 airport = st.selectbox("选择机场", list_airports())
 year = st.selectbox("选择年份", list_years(airport))
@@ -40,6 +40,7 @@ if st.button("查询图像"):
         file_name=f"{airport}_{year}_{choice}.png",
         mime="image/png"
     )
+
 
 
 
